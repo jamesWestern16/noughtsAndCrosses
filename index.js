c9b51815577aaf166c88fab33702308c1dbb32aa
James Western <jameswestern16@gmail.com>
2022-06-20 21:33:36 +0100

    Get placement working

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 console.log("Welcome to Noughts and Crosses");
 
-var prompt = require('prompt-sync')();
-//
-// get input from the user.
-//
-var n = prompt('How many more times? ');
+const prompt = require('prompt-sync')();
+// const n = prompt('How many more times? ');
 
-console.log(n);
+// console.log(n);
+
+let grid = [
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+]
+
+console.log(grid);
+
+// grid[1][1] = "x";
+
+console.log(grid);
+let row = prompt('which row would you like to choose?');
+let column = prompt('which column would you like to choose?');
+
+row = Number(row);
+column = Number(column);
+
+grid[row - 1][column - 1] = 'x';
+
+console.log(grid);

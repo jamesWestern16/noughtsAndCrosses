5cf467aea3684dd406cf72fd077c5b01d9fff13f
James Western <jameswestern16@gmail.com>
2022-06-20 21:18:21 +0100

    basic prompt working

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log("Welcome to Noughts and Crosses");
+
+var prompt = require('prompt-sync')();
+//
+// get input from the user.
+//
+var n = prompt('How many more times? ');
+
+console.log(n);
